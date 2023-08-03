822091fb84b38515fe727668a56a1ca17dfd5c17
f62264 <f62264@gmail.com>
2023-08-03 20:41:50 +0500

    changed main 1

--- Main.java
+++ Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         int x = 10;
-        int x2 = 100;
-        int x3 = 1000;
-        int x4 = 1000;
-        int x5 = 5555000;
     }
 }
